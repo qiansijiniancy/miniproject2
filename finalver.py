c20c81752260f964c18c6c11f5199a4618eb04ef
qiansijiniancy <sdyang522@gmail.com>
2019-02-16 16:54:01 -0500

    Add files via upload

--- finalver.py
+++ finalver.py
@@ -81,99 +81,209 @@ print(test_reviews.shape)
 '''
 Step 2: Preprocess Data
 '''
-train_reviews['review'] = train_reviews['review'].str.lower()
 
 
 # form corpus with train and test
 corpus = pd.concat([train_reviews,test_reviews], axis=0)
+corpus['review'] = corpus['review'].str.lower()
+
 print(corpus['review'].head(10))
+print(type(corpus['review']))
+print(len(corpus))
+### https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.astype.html
+### https://github.com/iworeushankaonce/Imdb/blob/master/Imdb_sentiment_polarity%20(1).ipynb
+### convert review contents to string & useful for pre-processing
+corpus['review'] = corpus['review'].astype(str)
+print(corpus['review'].iloc[5])
+import re
+def clean_htmlandsymbol(raw_text):
+  cleantext_nohtml = re.sub(re.compile('<.*?>|[0-9]'), '', raw_text)
+  cleantext_nosymbolandhtml = re.sub(re.compile('[\-\+!@#$%^&*()<>?()\|\/]'), '', cleantext_nohtml)
+  cleantext_nosymbolandhtml = cleantext_nosymbolandhtml.replace('<br>','')
+  cleantext_nosymbolandhtml = cleantext_nosymbolandhtml.replace('</br>', '')
+  return cleantext_nosymbolandhtml
 
-
-'''
-Step 3: Exact Features
-'''
-
-from sklearn.feature_extraction.text import CountVectorizer
-# Transform each text into a vector of word counts
-vectorizer = CountVectorizer(stop_words="english")
-
-training_features = vectorizer.fit_transform(train_reviews["review"])
-test_features = vectorizer.transform(test_reviews["review"])
-print(training_features.shape)
-
-
-'''
-Step 4: Model Training
-'''
-
-# Training and Test data
-# test_size = 0.5 cuz 25,000 + 25,000 =50,000
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(corpus['review'],
-                                                    corpus['target'],
-                                                    test_size=0.5)
-
-print(len(X_train), len(y_train), len(X_test), len(y_test))
+corpus['review'] = corpus['review'].apply(clean_htmlandsymbol)
 
 #
-# ####################
-# # Method 1: Multi Nominal NB
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import f1_score
-vec = CountVectorizer()
+# #### stemming
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+def stem_sentences(sentence):
+    tokens = sentence.split()
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+    return ' '.join(stemmed_tokens)
 
-X_train = vec.fit_transform(X_train)
-X_test = vec.transform(X_test)
-
-
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import f1_score
-
-#alpha parameters stands for Add-One(Laplace) Smooting
-model = MultinomialNB(alpha=1.0)
-
-model.fit(X_train, y_train)
-
-pred = model.predict(X_test)
-from sklearn.metrics import accuracy_score
-print('Accuracy: %.2f%%' % (accuracy_score(y_test, pred) * 100))
-print("The F1 accuracy score: {}%".format(f1_score(y_test, pred) * 100))
-
-##########
-#tf-idf
-########
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import f1_score
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-vec = TfidfVectorizer(min_df=3).fit(X_train)
-X_train_vectorized = vec.transform(X_train)
-model = MultinomialNB(alpha=1.0)
-model.fit(X_train_vectorized, y_train)
-from sklearn.metrics import accuracy_score
-
-y_pred = model.predict(vec.transform(X_test))
-print('Accuracy: %.2f%%' % (accuracy_score(y_test, y_pred) * 100))
+corpus['review'] = corpus['review'].apply(stem_sentences)
 #
+# #####lemma
+from nltk.stem.wordnet import WordNetLemmatizer
+lmtzr = WordNetLemmatizer()
+def lemmatize_sentences(sentence):
+    tokens = sentence.split()
+    lemmatized_tokens = [lmtzr.lemmatize(token) for token in tokens]
+    return ' '.join(lemmatized_tokens)
 
+corpus['review'] = corpus['review'].apply(lemmatize_sentences)
+
+corpus['review'].iloc[5]
 
 #
-# # ############################
-# # Method 2: SVM
-# from sklearn.svm import LinearSVC
-# from sklearn.metrics import f1_score
+# '''
+# Step 3: Exact Features
+# '''
+#
+# # from sklearn.feature_extraction.text import CountVectorizer
+# # # Transform each text into a vector of word counts
+# # vectorizer = CountVectorizer(stop_words="english")
+# #
+# # training_features = vectorizer.fit_transform(train_reviews["review"])
+# # test_features = vectorizer.transform(test_reviews["review"])
+# # print(training_features.shape)
+#
+#
+# '''
+# Step 4: Model Training
+# '''
+# # ####training model
+# from sklearn.model_selection import train_test_split
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# tfidf = TfidfVectorizer(binary=False, stop_words='english', max_features=150000, ngram_range = (1, 2))
+# X_train, X_validation, y_train, y_validation = train_test_split(train_reviews['review'],
+#                                                     train_reviews['target'],
+#                                                     train_size=0.95,
+#                                                     test_size=0.05)
 # from sklearn.feature_extraction.text import CountVectorizer
+# cv = CountVectorizer().fit(X_train)
+# X_train_counts = cv.transform(X_train)
+# X_validation_counts = cv.transform(X_validation)
+#
+# from sklearn.model_selection import train_test_split
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.feature_extraction.text import TfidfTransformer
+# from sklearn.preprocessing import Normalizer
+# from sklearn import metrics
+# from sklearn.svm import LinearSVC
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.naive_bayes import MultinomialNB
+# # tfidf
+# tfidf_transformer = TfidfTransformer().fit(X_train_counts)
+# X_train_tfidf = tfidf_transformer.transform(X_train_counts)
+# X_validation_tfidf = tfidf_transformer.transform(X_validation_counts)
+#
+# # normalization
+# normalizer_tranformer = Normalizer().fit(X=X_train_tfidf)
+# X_train_normalized = normalizer_tranformer.transform(X_train_tfidf)
+# X_validation_normalized = normalizer_tranformer.transform(X_validation_tfidf)
+#
+# def display_results(y_val, y_pred, heading):
+#     print(metrics.classification_report(y_val, y_pred))
+#     print("Accuracy % = ", metrics.accuracy_score(y_val, y_pred))
+#
+# clf_LR = LogisticRegression().fit(X_train_normalized, y_train)
+# y_pred = clf_LR.predict(X_validation_normalized)
+# display_results(y_validation, y_pred,"")
+#
+# clf_SVM = LinearSVC().fit(X_train_normalized, y_train)
+# y_pred = clf_SVM.predict(X_validation_normalized)
+# display_results(y_validation, y_pred,"")
+#
+#
+#
+# X_train, X_test, y_train, y_test = train_test_split(corpus['review'],
+#                                                     corpus['target'],
+#                                                     train_size=0.5,
+#                                                     test_size=0.5)
+# X_test_counts = cv.transform(X_test)
+# X_test_tfidf = tfidf_transformer.transform(X_test_counts)
+# X_test_normalized = normalizer_tranformer.transform(X_test_tfidf)
+# y_pred = clf_LR.predict(X_test_normalized)
+# display_results(y_test, y_pred,"")
+# from sklearn.metrics import f1_score
+# print("The F1 accuracy score: {}%".format(f1_score(y_test, y_pred) * 100))
+# pd.DataFrame(y_pred).to_csv("test_data_prediction_LR.csv")
+#
+#
+# y_pred = clf_SVM.predict(X_test_normalized)
+# display_results(y_test,y_pred,"")
+# from sklearn.metrics import f1_score
+# print("The F1 accuracy score: {}%".format(f1_score(y_test, y_pred) * 100))
+# pd.DataFrame(y_pred).to_csv("test_data_prediction_SVM.csv")
+# #
+# #
+# #
+# # # Training and Test data
+# # # test_size = 0.5 cuz 25,000 + 25,000 =50,000
+# # from sklearn.model_selection import train_test_split
+# # X_train, X_test, y_train, y_test = train_test_split(corpus['review'],
+# #                                                     corpus['target'],
+# #                                                     test_size=0.5)
+# #
+# # print(len(X_train), len(y_train), len(X_test), len(y_test))
+# #
+# # #
+# # # ####################
+# # # # Method 1: Multi Nominal NB
+# # from sklearn.feature_extraction.text import CountVectorizer
+# # from sklearn.metrics import f1_score
+# # vec = CountVectorizer()
+# #
+# # X_train = vec.fit_transform(X_train)
+# # X_test = vec.transform(X_test)
+# #
+# #
+# # from sklearn.naive_bayes import MultinomialNB
+# # from sklearn.metrics import f1_score
+# #
+# # #alpha parameters stands for Add-One(Laplace) Smooting
+# # model = MultinomialNB(alpha=1.0)
+# #
+# # model.fit(X_train, y_train)
+# #
+# # pred = model.predict(X_test)
+# # from sklearn.metrics import accuracy_score
+# # #print('Accuracy: %.2f%%' % (accuracy_score(y_test, pred) * 100))
+# # print("The F1 accuracy score: {}%".format(f1_score(y_test, pred) * 100))
+# #
+# # pd.DataFrame(pred).to_csv("prediction.csv")
+#
+# ##########
+# #tf-idf
+# # ########
+# # from sklearn.naive_bayes import MultinomialNB
+# # from sklearn.metrics import f1_score
+# # from sklearn.feature_extraction.text import TfidfVectorizer
+# #
+# # vec = TfidfVectorizer(min_df=3).fit(X_train)
+# # X_train_vectorized = vec.transform(X_train)
+# # model = MultinomialNB(alpha=1.0)
+# # model.fit(X_train_vectorized, y_train)
+# # from sklearn.metrics import accuracy_score
+# #
+# # y_pred = model.predict(vec.transform(X_test))
+# # print('Accuracy: %.2f%%' % (accuracy_score(y_test, y_pred) * 100))
+# # pd.DataFrame(y_pred).to_csv("tf_nb_prediction.csv")
+# # #
+#
+#
+# #
+# # # ############################
+# # # Method 2: SVM
+# # from sklearn.svm import LinearSVC
+# # from sklearn.metrics import f1_score
+# # from sklearn.feature_extraction.text import CountVectorizer
 # # vec = CountVectorizer()
 # # X_train = vec.fit_transform(X_train)
 # # X_test = vec.transform(X_test)
-#
-# model = LinearSVC(C=4,loss='squared_hinge')
-# model.fit(X_train, y_train)
-# pred = model.predict(X_test)
-#
-# print("The F1 accuracy score: {}%".format(f1_score(y_test, pred) * 100))
-#
-# pd.DataFrame(pred).to_csv("SVM_prediction.csv")
-# print(X_train.shape)
-# print(y_train.shape)
-# print(X_test.shape)
+# #
+# # model = LinearSVC(C=4,loss='squared_hinge')
+# # model.fit(X_train, y_train)
+# # pred = model.predict(X_test)
+# #
+# # print("The F1 accuracy score: {}%".format(f1_score(y_test, pred) * 100))
+# #
+# # pd.DataFrame(pred).to_csv("SVM_prediction.csv")
+# # print(X_train.shape)
+# # print(y_train.shape)
+# # print(X_test.shape)
