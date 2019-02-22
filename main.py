bfb15fd5ee7277491b18f83988e076fe3790655c
qiansijiniancy <sdyang522@gmail.com>
2019-02-22 00:00:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 ### Mini project 2
+### Comp 551, McGill University
+### author: Sidi Yang
 
 import os
 import pandas as pd
@@ -6,8 +8,9 @@ import nltk
 import re
 import numpy as np
 from nltk.stem import PorterStemmer
-from nltk.tokenize import WordPunctTokenizer
-# author: Sidi Yang
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+
 
 '''
 Step 1: Load Data
@@ -15,155 +18,97 @@ Step 1: Load Data
 # current path
 cwd = os.getcwd()
 
-#read pos/neg files
+# read pos files
 positive_folder =[]
-for x in os.listdir(cwd + '/data/train/pos/'):
+pos_path = cwd + '/data/train/pos/'
+for x in os.listdir(pos_path):
     if x.endswith(".txt"):
-
         positive_folder.append(x)
+positiveReviews = []
+for pfile in positive_folder:
+    with open(pos_path+pfile, encoding="utf8") as f:
+        positiveReviews.append(f.read())
 
+# read neg files
 negative_folder =[]
-for x in os.listdir(cwd + '/data/train/neg/'):
+neg_path = cwd + '/data/train/neg/'
+for x in os.listdir(neg_path):
     if x.endswith(".txt"):
         negative_folder.append(x)
-
-# test_folder =[]
-# for x in os.listdir(cwd + '/data/test/'):
-#     if x.endswith(".txt"):
-#         test_folder.append(x)
-
-test_review_file = pd.read_csv('/Users/cindyang/Desktop/Imdb_sentiment_analysis/test.csv',encoding="latin1")
-
-###################
-
-positiveReviews, negativeReviews, testReviews = [], [], []
-
-for pfile in positive_folder:
-    with open(cwd+"/data/train/pos/"+pfile, encoding="utf8") as f:
-        positiveReviews.append(f.read())
+negativeReviews = []
 for nfile in negative_folder :
-    with open(cwd+"/data/train/neg/"+nfile, encoding="utf8") as f:
+    with open(neg_path+nfile, encoding="utf8") as f:
         negativeReviews.append(f.read())
-# for tfile in test_folder:
-#     with open(cwd+"/data/test/"+tfile, encoding="utf8") as f:
-#         testReviews.append(f.read())
 
-# train_reviews
+# read test files
+# ref: https://cs.mcgill.ca/~wlh/comp551/slides/12-ensembles.pdf
+def read_test():
+    data = []
+    folder = cwd + "/data/test/"
+    for i in range(25000):
+        with open(os.path.join(folder, str(i)+".txt"), "rb") as f:
+            review = f.read().decode("utf-8").replace('\n','').strip().lower()
+            data.append([review,i])
+    return data
+
+testReviews = read_test()
+###################
+### make dataframe for train and test
 train_reviews = pd.concat([
     pd.DataFrame({"review":positiveReviews, "target":1}),
     pd.DataFrame({"review":negativeReviews, "target":0}),
 ])
-#],
-# ignore_index=True).sample(frac=1, random_state=1)
-
-#
-# pos_reviews = pd.concat([
-#     pd.DataFrame({"review":positiveReviews, "target":1}),
-#     #pd.DataFrame({"review":negativeReviews, "target":0}),
-# #])
-#  ],
-#      ignore_index=True).sample(frac=1, random_state=1)
-#
-# neg_reviews = pd.concat([
-#     #pd.DataFrame({"review":positiveReviews, "target":1}),
-#     pd.DataFrame({"review":negativeReviews, "target":0}),
-# #])
-#  ],
-#      ignore_index=True).sample(frac=1, random_state=1)
 
 test_reviews = pd.concat([
-    pd.DataFrame({"review":test_review_file["review"], "target":0})])
-# prove it works
-print(train_reviews.shape)
-
-test_reviews = test_reviews.sort_index()
-print(test_reviews['review'].head(10))
-print(test_reviews['review'].tail(10))
+    # set targets of test reviews to 0 for now
+    pd.DataFrame({"review":testReviews, "target":0})])
 
 
 '''
 Step 2: Preprocess Data
 '''
-# form corpus with train and test
-corpus = pd.concat([train_reviews,test_reviews], axis=0)
-corpus['review'] = corpus['review'].str.lower()
-
-print(corpus['review'].head(10))
-print(type(corpus['review']))
-print(len(corpus))
-### https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.astype.html
-### https://github.com/iworeushankaonce/Imdb/blob/master/Imdb_sentiment_polarity%20(1).ipynb
-### convert review contents to string & useful for pre-processing
-corpus['review'] = corpus['review'].astype(str)
+### convert to strings for pre-processing
 train_reviews['review'] = train_reviews['review'].astype(str)
 test_reviews['review'] = test_reviews['review'].astype(str)
 
-import re
-def clean_htmlandsymbol(raw_text):
-  cleantext_nohtml = re.sub(re.compile('<.*?>|[0-9]'), '', raw_text)
-  cleantext_nosymbolandhtml = re.sub(re.compile('[\-\+!@#$%^&*()<>?()\|\/]'), '', cleantext_nohtml)
-  cleantext_nosymbolandhtml = cleantext_nosymbolandhtml.replace('<br>','')
-  cleantext_nosymbolandhtml = cleantext_nosymbolandhtml.replace('</br>', '')
+def clean_text(raw_text):
+  cleantext = re.sub(re.compile('<.*?>|[0-9]'), '', raw_text)
+  cleantext = re.sub(re.compile('[\-\+!@#$%^&*()<>?()\|\/]'), '', cleantext)
+  cleantext = cleantext.replace('<br>','')
+  cleantext= cleantext.replace('</br>', '')
 
-  return cleantext_nosymbolandhtml
-corpus['review'] = corpus['review'].apply(clean_htmlandsymbol)
-train_reviews['review'] = train_reviews['review'].apply(clean_htmlandsymbol)
-test_reviews['review'] = test_reviews['review'].apply(clean_htmlandsymbol)
+  return cleantext
+train_reviews['review'] = train_reviews['review'].apply(clean_text)
+test_reviews['review'] = test_reviews['review'].apply(clean_text)
 #
-# #### stemming
-from nltk.stem import PorterStemmer
-stemmer = PorterStemmer()
-def stem_sentences(sentence):
-    tokens = sentence.split()
-    stemmed_tokens = [stemmer.stem(token) for token in tokens]
-    return ' '.join(stemmed_tokens)
+# # #### stemming and lemmatization
+ps = PorterStemmer()
+lem = WordNetLemmatizer()
 
-corpus['review'] = corpus['review'].apply(stem_sentences)
-train_reviews['review'] = train_reviews['review'].apply(stem_sentences)
-test_reviews['review'] = test_reviews['review'].apply(stem_sentences)
-#
-# #####lemma
-from nltk.stem.wordnet import WordNetLemmatizer
-lmtzr = WordNetLemmatizer()
-def lemmatize_sentences(sentence):
-    tokens = sentence.split()
-    lemmatized_tokens = [lmtzr.lemmatize(token) for token in tokens]
-    return ' '.join(lemmatized_tokens)
+def stem_reviews(review):
+    words = review.split()
+    words = [ps.stem(word) for word in words]
+    return ' '.join(words)
 
-corpus['review'] = corpus['review'].apply(lemmatize_sentences)
+def lemmatize_reviews(review):
+    words = review.split()
+    lemmatized_words = [lem.lemmatize(word) for word in words]
+    return ' '.join(lemmatized_words)
 
-corpus['review'] = corpus['review'].apply(lemmatize_sentences)
-train_reviews['review'] = train_reviews['review'].apply(lemmatize_sentences)
-test_reviews['review'] = test_reviews['review'].apply(lemmatize_sentences)
-
+train_reviews['review'] = train_reviews['review'].apply(stem_reviews)
+train_reviews['review'] = train_reviews['review'].apply(lemmatize_reviews)
+test_reviews['review'] = test_reviews['review'].apply(stem_reviews)
+test_reviews['review'] = test_reviews['review'].apply(lemmatize_reviews)
 
 ### stopwords remove
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-
-en_words = set(stopwords.words('english'))
-corpus['review'] = [' '.join([w for w in x.lower().split() if w not in en_words])
-    for x in corpus['review'].tolist()]
-
-
-en_words = set(stopwords.words('english'))
-train_reviews['review'] = [' '.join([w for w in x.lower().split() if w not in en_words])
-    for x in train_reviews['review'].tolist()]
-
-en_words = set(stopwords.words('english'))
-test_reviews['review'] = [' '.join([w for w in x.lower().split() if w not in en_words])
-    for x in test_reviews['review'].tolist()]
-
-
-print(corpus['review'].head(10))
-print(corpus['review'].iloc[10])
-print(test_reviews['review'].head(20))
-print(test_reviews['review'].iloc[20])
-
-print(type(corpus))
-print(type(test_reviews))
-print(corpus['review'].tail(10))
-
+### consider to remove this part as it is not improving at all lol!!!
+# en_words = set(stopwords.words('english'))
+# train_reviews['review'] = [' '.join([w for w in x.lower().split() if w not in en_words])
+#     for x in train_reviews['review'].tolist()]
+#
+# en_words = set(stopwords.words('english'))
+# test_reviews['review'] = [' '.join([w for w in x.lower().split() if w not in en_words])
+#     for x in test_reviews['review'].tolist()]
 
 '''
 Step 3: Model Training
@@ -182,7 +127,7 @@ cv = CountVectorizer(binary=True, stop_words='english',ngram_range=(1, 2)).fit(X
 X_train_counts = cv.transform(X_train)
 X_validation_counts = cv.transform(X_validation)
 
-from sklearn.model_selection import train_test_split
+
 from sklearn import metrics
 from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
@@ -204,22 +149,26 @@ X_train_normalized = normalizer_tranformer.transform(X_train_tfidf)
 X_validation_normalized = normalizer_tranformer.transform(X_validation_tfidf)
 
 # use metrics.classification_report to present the results
-def display_results(true_result, pred_result):
+def display_results(true_result, pred_result,head):
     print(metrics.classification_report(true_result, pred_result))
     print("Accuracy % = ", metrics.accuracy_score(true_result, pred_result))
 
 # models
 clf_LR = LogisticRegression().fit(X_train_normalized, y_train)
-y_vali1_pred = clf_LR.predict(X_train_normalized)
-display_results(y_validation, y_vali1_pred,"")
+y_vali1_pred = clf_LR.predict(X_validation_normalized)
+display_results(y_validation, y_vali1_pred,"Logistic Regression Results")
 
 clf_MB = MultinomialNB(alpha=1.0).fit(X_train_normalized, y_train)
-y_vali2_pred = clf_MB.predict(X_train_normalized)
-display_results(y_validation, y_vali2_pred,"")
+y_vali2_pred = clf_MB.predict(X_validation_normalized)
+display_results(y_validation, y_vali2_pred,"Multi Naive Bayes Results")
 
 clf_SVM = LinearSVC().fit(X_train_normalized, y_train)
-y_vali3_pred = clf_SVM.predict(X_train_normalized)
-display_results(y_validation, y_vali3_pred,"")
+y_vali3_pred = clf_SVM.predict(X_validation_normalized)
+display_results(y_validation, y_vali3_pred,"SVM Results")
+
+clf_DT = DecisionTreeClassifier().fit(X_train_normalized, y_train)
+y_vali4_pred = clf_DT.predict(X_validation_normalized)
+display_results(y_validation, y_vali4_pred,"Decision Trees Results")
 
 # ### prediction and evaluation
 # from sklearn import metrics
@@ -238,23 +187,22 @@ display_results(y_validation, y_vali3_pred,"")
 #     target_names= train_reviews.target_names))
 
 
-
-### here needs to be test on Feb 18:
 X_test = test_reviews['review']
 X_test_counts = cv.transform(X_test)
 X_test_tfidf = tfidf_transformer.transform(X_test_counts)
 X_test_normalized = normalizer_tranformer.transform(X_test_tfidf)
 
-y_test1_pred = clf_LR.predict(X_train_normalized)
+y_test1_pred = clf_LR.predict(X_test_normalized)
 pd.DataFrame(y_test1_pred).to_csv("test_data_prediction_LR.csv")
 
-y_test2_pred = clf_MB.predict(X_train_normalized)
-#pd.DataFrame(y_pred).columns = ['Id','Category']
+y_test2_pred = clf_MB.predict(X_test_normalized)
 pd.DataFrame(y_test2_pred).to_csv("test_data_prediction_MNB.csv")
 
-y_test3_pred = clf_SVM.predict(X_train_normalized)
-#pd.DataFrame(y_pred).columns = ['Id','Category']
+y_test3_pred = clf_SVM.predict(X_test_normalized)
 pd.DataFrame(y_test3_pred).to_csv("test_data_prediction_SVM.csv")
+
+y_test3_pred = clf_DT.predict(X_test_normalized)
+pd.DataFrame(y_test3_pred).to_csv("test_data_prediction_DT.csv")
 
 print(X_test.iloc[12])
 print(type(test_reviews))
